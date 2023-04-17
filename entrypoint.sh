d6f917e2336998c9e5ce4a2856e83e8bebc9d073
Chekoté <domhnall.tilliere@gmail.com>
2023-04-17 10:31:59 -0500

    Double quote to prevent globbing and word splitting.
    See https://github.com/koalaman/shellcheck/wiki/SC2086

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,10 @@
 # Based on: Deni Bertović https://denibertovic.com/posts/handling-permissions-with-docker-volumes/
 
 # Use the LOCAL_USER_ID that was passed in, or fallback to 9001
-USER_ID=${LOCAL_USER_ID:-9001}
+USER_ID="${LOCAL_USER_ID:-9001}"
 
 # Get the username that is associated with the user id (if any)
-USER_NAME=$(getent passwd | awk -F: '$3 == '${USER_ID}' { print $1 }')
+USER_NAME="$(getent passwd | awk -F: '$3 == '"${USER_ID}"' { print $1 }')"
 
 # Does the user already exist?
 if [ "$USER_NAME" == "" ]; then
@@ -19,12 +19,12 @@ if [ "$USER_NAME" == "" ]; then
   fi
 
   # Create the user.
-  useradd --shell /bin/bash -u ${USER_ID} ${ARGS} user
-  export HOME=/home/${USER_NAME}
+  useradd --shell /bin/bash -u "${USER_ID}" ${ARGS} user
+  export HOME="/home/${USER_NAME}"
 
   # Ensure home is owned by user (Docker may have created it as root when mounting volumes)
-  chown ${USER_NAME} $HOME
+  chown "${USER_NAME}" $HOME
 fi
 
 # Execute the command
-gosu ${USER_NAME} "$@"
+gosu "${USER_NAME}" "$@"
